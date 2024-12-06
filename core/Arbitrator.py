006f70446c7208cc52f44b806abb0c6dd69e5e19
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-12-06 09:15:57 +0100

    feat: Arbitator structure

---
+++ core/Arbitrator.py
@@ -0,0 +1,30 @@
+# arbitator.py
+
+class Arbitator:
+    '''
+    Arbitrator class counts the score of a solution.
+
+    Attributes:
+        score (int): The score of the solution.
+    '''
+    def __init__(self):
+        '''
+        The constructor for Arbitrator class.
+        '''
+        self.score: int = 0
+
+    def turn_score(self, balloons: list[list[int]]) -> int:
+        '''
+        The function to count the score of a solution.
+
+        Args:
+            balloons (list[list[int]]): The list of balloons.
+        '''
+        return
+
+    def get_score(self) -> int:
+        '''
+        The function to return the score of the solution.
+        '''
+        return self.score
+    
