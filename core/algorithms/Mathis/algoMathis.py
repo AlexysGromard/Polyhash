187c1916734c67d6786fc8ea40506042ebc1eed6
Mathis RABUSSEAU <mathis.rabusseau@etu.univ-nantes.fr>
2024-12-06 12:37:31 +0100

    feat: Premier algo aléatoire fonctionnel

--- core/algorithms/Mathis/algoMathis.py
+++ core/algorithms/Mathis/algoMathis.py
@@ -1,89 +1,111 @@
 import random
-
-class Vector3:
-
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __add__(self, other):
-        if isinstance(other, Vector3):
-            return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
-        return NotImplemented
-
-    def __iadd__(self, other):
-        if isinstance(other, Vector3):
-            self.x += other.x
-            self.y += other.y
-            self.z += other.z
-            return self
-        return NotImplemented
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y and self.z == other.z
-
+from core.models import DataModel,Vector3
 
 class AlgoMathis:
 
     def compute(self, data: 'DataModel'):
-        return self.process(data)
+        d = self.process(data)
+        while d == False:
+            d = self.process(data)
+        return d
 
     def __convertData(self):
         pass
 
-    def process(self, d: 'DataModel'):
+    def process(self, d:' DataModel'):
 
         #Stocke l'historique
         trajet = []
 
         #Stocke les positions des ballons
-        balloons = [Vector3(d.startCell.x, d.startCell.y, 0) * d.nbBalloons]
+        balloons = [Vector3(d.starting_cell.x, d.starting_cell.y, 0) for i in range(d.num_balloons)]
 
         #Boucle principale
         for turn in range(d.turns):
-
+            
+            #Stocke la ligne des variations de positions des ballons
             trajetRow = []
 
             for balloon in balloons:
+                print(f"{turn}:({balloon.x}, {balloon.y}, {balloon.z})")
                 #Changement altitude
                 altChange = self.randomAlt(d, balloon)
 
-                trajetRow.append(altChange)
-
+               
+                old = balloon.z
                 balloon.z+= altChange
 
-                balloon = self.nextPlace(d, balloon)
+                b = self.nextPlace(d, balloon)
+                if b == False:
+                    i = 0
+                    while i < 100:
+                        i+= 1
+                        
+                        balloon.z = old
+                        altChange = self.randomAlt(d, balloon)           
+                        balloon.z+= altChange
+                        b = self.nextPlace(d, balloon)
+                        if b != False:
+                           i = 10000
+                        
+                    return False
+                balloon.x = b.x
+                balloon.y = b.y
+                balloon.z = b.z
+                trajetRow.append(altChange)
+                print(f"{turn}:({balloon.x}, {balloon.y}, {balloon.z})")
+                
+            
             trajet.append(trajetRow)
         return trajet
     
-    def randomAlt(self,d: 'DataModel', place: Vector3) -> 'Vector3':
-        """Retourne une altitude random correcte pour le ballon"""
-        correct = []
-        for i in range(-1, 3):
-            if 0 < place.z + i < d.alts:
-                correct.append(i)
-        place.z += correct[random.randint(0, len(correct))]
+    def randomAlt(self,d: 'DataModel', place: 'Vector3') -> int:
+        """Calculate a CORRECT random variation of altitude for the balloon. 
 
-    def nextPlace(self, d: 'DataModel', place: Vector3) -> Vector3 : 
+        Args:
+            d (DataModel): DataModel (to do not exceed max alt)
+            place (Vector3): actual position of the balloon (just the Z coord. is used)
+
+        Returns:
+            int: -1|0|1 Correct altitude variation for this balloon (can not be 0 or superior to the max alt).
         """
-        Retourne la nouvelle place d'un ballon en fonction de sa position place(Vector3)
-        et des vents présents dans d(DataModel)
-        Retourne False si le ballon sort de l'écran.    
+        correct = []
+        for i in range(-1, 2):
+            if 0 < place.z + i < d.altitudes:
+                correct.append(i)
+        return correct[random.randint(0, len(correct) - 1)]
+
+    def nextPlace(self, d: 'DataModel', place: 'Vector3') : 
+        """Compute the new position of the balloon after the wind.
+
+        Args:
+            d (DataModel): DataModel in order to get the winds.
+            place (Vector3): Actual position of the balloon (Z coord. matter)
+
+        Returns:
+            Vector3 | False: Return the new position of the balloon OR false if the balloon quit the grid.
         """
         
 
         #Check si l'altitude est correcte
-        if place.z > d.nbWinds:
+        if place.z > d.altitudes:
             return False
+        
         
         #Changement de position
-        wind = d.winds[place.z][place.x][place.y]
-        place += wind
-        place.x = place.x % d.cols
+        wind = d.wind_grids[place.z - 1][place.x][place.y]
+        print(wind)
+        new = Vector3(
+            place.x +wind.x,
+            place.y +wind.y,
+            place.z +wind.z,
+        )
+        new.y = new.y % d.cols
 
         #Check si le ballon ne sort pas en haut / en bas
-        if place.y < 0 or place.y >= d.rows:
+        
+        if new.x < 0 or new.x >= d.rows:
+           
             return False
         
-        return place
+        return new
