32b816dc68036786b2c030455cd206d1b9b26de4
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-12-11 18:59:09 +0100

    doc: Add comments with the tests

--- tests/test_arbitrator.py
+++ tests/test_arbitrator.py
@@ -7,23 +7,42 @@ from core.Arbitrator import Arbitrator
 from core.models import *
 
 class TestArbitratorInit(unittest.TestCase):
+    '''
+    Test the Arbitrator class constructor
+    '''
     def test_no_args(self):
-        # Test the case where no arguments are passed in argument
+        '''
+        Test the case where no arguments are passed in argument
+
+        Expected: TypeError
+        '''
         with self.assertRaises(TypeError):
             Arbitrator()
 
     def test_invalid_grid_size(self):
-        # Test the case where the grid size is invalid
+        '''
+        Test the case where the grid size is invalid
+
+        Expected: ValueError
+        '''
         with self.assertRaises(ValueError):
             Arbitrator(0, 0)
 
     def test_negative_grid_size(self):
-        # Test the case where the grid size is negative
+        '''
+        Test the case where the grid size is negative
+
+        Expected: ValueError
+        '''
         with self.assertRaises(ValueError):
             Arbitrator(-1, -1)
 
     def test_valid_grid_size(self):
-        # Test the case where the grid size is valid
+        '''
+        Test the case where the grid size is valid
+
+        Expected: The coverage map is initialized correctly
+        '''
         for values_in, values_out in [
             ((1, 1), [[0]]),
             ((2, 2), [[0, 0], [0, 0]]),
@@ -36,26 +55,45 @@ class TestArbitratorInit(unittest.TestCase):
                 self.assertEqual(arbitrator.get_coverage_map(), values_out)
 
 class TestTurn_score(unittest.TestCase):
+    '''
+    Test the turn_score method of the Arbitrator class
+    '''
     def test_no_args(self):
-        # Test the case where no arguments are passed in argument
+        '''
+        Test the case where no arguments are passed in argument
+
+        Expected: TypeError
+        '''
         arbitrator = Arbitrator(1, 1)
         with self.assertRaises(TypeError):
             arbitrator.turn_score()
 
     def test_empty_args(self):
-        # Test the case where the arguments are empty
+        '''
+        Test the case where the arguments are empty
+
+        Expected: TypeError
+        '''
         arbitrator = Arbitrator(1, 1)
         with self.assertRaises(TypeError):
             arbitrator.turn_score([], [], -2)
 
     def test_radius_negative(self):
-        # Test the case where the radius is negative
+        '''
+        Test the case where the radius is negative
+
+        Expected: TypeError
+        '''
         arbitrator = Arbitrator(1, 1)
         with self.assertRaises(TypeError):
             arbitrator.turn_score([Vector3(0, 0, 0)], [Vector3(0, 0, 0)], -2)
 
     def test_radius_1(self):
-        # Test the case where the grid is 10x10 and the radius is 1
+        '''
+        Test the case where the grid is 10x10 and the radius is 1
+
+        Expected: 4
+        '''
         arbitrator = Arbitrator(10, 10)
         ballons = [
             Vector3(2, 3),
@@ -76,7 +114,11 @@ class TestTurn_score(unittest.TestCase):
         self.assertEqual(res, 4)
 
     def test_radius_2(self):
-        # Test the case where the grid is 10x10 and the radius is 2
+        '''
+        Test the case where the grid is 10x10 and the radius is 2
+
+        Expected: 5
+        '''
         arbitrator = Arbitrator(10, 10)
         ballons = [
             Vector3(5, 5),
@@ -95,7 +137,11 @@ class TestTurn_score(unittest.TestCase):
         self.assertEqual(res, 5)
     
     def test_subject(self):
-        # Test the case where the grid is 3x5 and the radius is 2 for 5 turns
+        '''
+        Test the case where the grid is 3x5 and the radius is 2 for 5 turns
+
+        Expected: 5
+        '''
         arbitrator = Arbitrator(3, 5)
         ballons_per_turn = [
             Vector3(1, 3),
@@ -119,7 +165,11 @@ class TestTurn_score(unittest.TestCase):
         self.assertEqual(total_score, 5)
 
     def test_two_ballons_on_same_target_coverage_1(self):
-        # Test the case where two ballons are on the same target
+        '''
+        Test the case where two ballons are on the same target
+
+        Expected: 2
+        '''
         arbitrator = Arbitrator(10, 10)
         ballons = [
             Vector3(4, 4),
@@ -136,7 +186,11 @@ class TestTurn_score(unittest.TestCase):
         self.assertEqual(res, 2)
 
     def test_two_ballons_on_same_target_coverage_2(self):
-        # Test the case where two ballons are on the same target
+        '''
+        Test the case where two ballons are on the same target
+
+        Expected: 3
+        '''
         arbitrator = Arbitrator(10, 10)
         ballons = [
             Vector3(4, 4),
