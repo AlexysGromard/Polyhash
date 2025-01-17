44ed60345032a89e80f3c9b2988501483984abb4
Mathis RABUSSEAU <mathis.rabusseau@etu.univ-nantes.fr>
2025-01-17 14:01:41 +0100

    feat(ECM/Combi): Ajoute la structure de base de Combi.py (non fonctionnel)

--- core/algorithms/ECM/Combi.py
+++ core/algorithms/ECM/Combi.py
@@ -6,6 +6,16 @@ class Combi:
         pass
 
     def process(self, explorerData: list[tuple[list[int], list[Vector3], int]]) -> tuple[list[int], list[Vector3], int]:
-        
+        """Genère une bonne selection de ballons parmits les exploreurs passés en arguments
+
+        Args:
+            explorerData (list[tuple[list[int], list[Vector3], int]]): Liste de tuple 
+            des exploreurs comme suit: (Path, BalloonPosition, score)
+
+        Returns:
+            tuple[list[int], list[Vector3], int]: (Path, BalloonPosition, score)
+        """
         pass
 
+
+    
