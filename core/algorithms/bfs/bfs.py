46cc138ee1e524c3b6cd6765cc728619ac28f8b2
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-01-17 08:35:01 +0100

    feat: Génération du graphe par l'algo BFS

--- core/algorithms/bfs/bfs.py
+++ core/algorithms/bfs/bfs.py
@@ -33,18 +33,30 @@ class BFS(Algorithm):
         Returns:
         """
         # Création des arêtes
+        print("Generating graph")
         self.graph = {}
-        for row in range(self.data.height):
-            for col in range(self.data.width):
+        for row in range(self.data.rows):
+            for col in range(self.data.cols):
                 for alt in range(self.data.altitudes):
                     self.graph[(row, col, alt)] = []
 
                     # Récupérer le vent
-                    wind = self.data.wind_grids[row][col][alt]
+                    wind = self.data.wind_grids[alt][row][col]
+
+                    # Récupérer la nouvelle position
+                    new_row = row + wind.x
+                    new_col = col + wind.y
+
+                    # Si la nouvelle position n'est pas dans la grille, on ne l'ajoute pas
+                    if new_row < 0 or new_row >= self.data.rows or new_col < 0 or new_col >= self.data.cols:
+                        continue
 
                     # Récupérer le nombre de points obtenus si l'on se rend à cette position
-                    points = self.arbitrator.
+                    points = self.arbitrator.score_for_ballon(Vector3(new_row, new_col, alt)) # C'est le poids de l'arête
 
+                    # Ajouter l'arête
+                    self.graph[(row, col, alt)].append(((row, col, alt), points))
+        print("Graph generated")
 
     def compute(self) -> list[list[int]]:
         """
@@ -55,7 +67,8 @@ class BFS(Algorithm):
         """
 
         self.graph_generation()
-        return self.trajet
+        print(self.graph)
+        return 
 
     def _convert_data(self) -> None:
         """
