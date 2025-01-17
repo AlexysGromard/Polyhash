38aaa0946ffde21ed5044eaf7a93dfdc76e03151
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-01-17 22:01:40 +0100

    feat: ajouter l'algorithme WPGMA pour la création de clusters de cibles

--- core/algorithms/CBBA/CBBA.py
+++ core/algorithms/CBBA/CBBA.py
@@ -17,6 +17,62 @@ class CBBA(Algorithm):
         '''
         return ((a[0] - b[0])**2 + (a[1] - b[1])**2) ** 0.5
 
+    def wpgma(self, targets, coverage_radius):  # TODO : À TESTER
+        '''
+        Compute the WPGMA algorithm to create clusters of targets.
+
+        Args:
+            targets (list[Vector3]): list of targets
+            coverage_radius (float): coverage radius of the balloons
+        Returns:
+            clusters (list[list[Vector3]]): list of clusters
+        '''
+        # Initialize the clusters
+        clusters = [[target] for target in targets]
+
+        # Initialize the distance matrix
+        distance_matrix = [[self.euclidean_distance((t1.x, t1.y), (t2.x, t2.y)) 
+                            for t2 in targets] for t1 in targets]
+
+        while len(clusters) > 1:
+            # Find the pair of clusters with the smallest distance
+            min_distance = float('inf')
+            merge_i, merge_j = -1, -1
+
+            for i in range(len(clusters)):
+                for j in range(i + 1, len(clusters)):
+                    # Compute the average distance between clusters[i] and clusters[j]
+                    cluster_distance = sum(
+                        distance_matrix[targets.index(clusters[i][k])][targets.index(clusters[j][l])] 
+                        for k in range(len(clusters[i])) 
+                        for l in range(len(clusters[j]))
+                    ) / (len(clusters[i]) * len(clusters[j]))
+
+                    if cluster_distance < min_distance:
+                        min_distance = cluster_distance
+                        merge_i, merge_j = i, j
+
+            # Check if the distance is less than the coverage radius
+            if min_distance > coverage_radius:
+                break
+
+            # Merge the two clusters
+            clusters[merge_i] += clusters[merge_j]
+            clusters.pop(merge_j)
+
+            # Update the distance matrix
+            for i in range(len(clusters)):
+                if i == merge_i:
+                    continue
+                new_distance = sum(
+                    self.euclidean_distance((p1.x, p1.y), (p2.x, p2.y))
+                    for p1 in clusters[merge_i]
+                    for p2 in clusters[i]
+                ) / (len(clusters[merge_i]) * len(clusters[i]))
+                distance_matrix[merge_i][i] = distance_matrix[i][merge_i] = new_distance
+
+        return clusters
+
     def create_clusters(self, targets, coverage_radius): # TODO : A TESTER
         '''
         Create clusters of targets
@@ -28,27 +84,8 @@ class CBBA(Algorithm):
             clusters (list[list[int]]): list of clusters
             cluster_centers (list[Vector3]): list of cluster
         '''
-        clusters = []
-        visited = set()
-
-        for target in targets:
-            if target in visited:
-                continue
-
-            # Create a new cluster
-            cluster = [target]
-            visited.add(target)
-
-            # Add all targets within the coverage radius
-            for target2 in targets:
-                if target2 in visited:
-                    continue
-
-                if self.euclidean_distance((target.x, target.y), (target2.x, target2.y)) <= coverage_radius:
-                    cluster.append(target2)
-                    visited.add(target2)
-
-            clusters.append(cluster)
+        # Mettre les targets sous forme de points (x, y)
+        clusters = self.wpgma(targets, coverage_radius)
 
         # Calculate the center of each cluster
         cluster_centers = []
