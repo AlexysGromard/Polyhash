2a83be380086b4997dbcbfa930fa801ca88b250c
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-01-17 13:40:08 +0100

    feat: ajouter la gestion des mouvements des ballons et le calcul des scores dans l'algorithme BFS

--- core/algorithms/bfs/bfs.py
+++ core/algorithms/bfs/bfs.py
@@ -1,5 +1,7 @@
 # bfs.py
 
+import random
+
 from collections import deque
 
 from ...models import DataModel,Vector3
@@ -67,8 +69,53 @@ class BFS(Algorithm):
         """
 
         self.graph_generation()
-        print(self.graph)
-        return 
+
+        # Init des variables
+        total_points = 0
+        trajectory = [[] for _ in range(self.data.turns)]
+
+        # Initialisation des ballons
+        ballons = []
+        for ballon in range(self.data.num_balloons):
+            ballons.append(Vector3(self.data.starting_cell.x, self.data.starting_cell.y, 0))
+
+        for turn in range(self.data.turns):
+            itinary = [] # Liste des mouvements des ballons (pour ne pas faire aller 2 ballons sur la même case)
+            for i, ballon in enumerate(ballons):
+                # On cherche pour la position du ballon, l'altitude qui maximise le score (dans self.graph)
+                max_score = 0
+                best_alt = 0
+                for alt in range(self.data.altitudes):
+                    if self.graph[(ballon.x, ballon.y, alt)] and self.graph[(ballon.x, ballon.y, alt)][0][1] > max_score and (ballon.x, ballon.y, alt) not in itinary:
+                        max_score = self.graph[(ballon.x, ballon.y, alt)][0][1]
+                        best_alt = alt
+                # Si best_alt == 0, prendre une altitude aléatoire
+                if best_alt == 0:
+                    for _ in range(self.data.altitudes):
+                        best_alt = random.randint(0, self.data.altitudes - 1)
+                        # Vérifier si l'altitude n'a pas déjà été prise et qu'il y a une arête qui part de cette altitude
+                        if (ballon.x, ballon.y, best_alt) not in itinary and self.graph[(ballon.x, ballon.y, best_alt)]:
+                            break
+
+                # Ajouter l'intinéraire
+                itinary.append((ballon.x, ballon.y, best_alt))
+
+                # Ajouter le mouvement à la trajectoire
+                # print(f"Ballon {i} : {ballon.x} {ballon.y} {ballon.z} -> {ballon.x} {ballon.y} {best_alt}")
+                trajectory[turn].append(best_alt - ballon.z)
+
+                # Mettre à jour la position du ballon
+                ballon.z = best_alt
+
+                # Déplacer le ballon
+                self.data.updatePositionWithWind(ballon) # Mettre à jour la position du ballon directement dans le paramètre
+
+                ballons[i] = Vector3(ballon.x, ballon.y, ballon.z)
+
+            # Calculer le score
+            total_points += self.arbitrator.turn_score(ballons)
+        print(f"Total points: {total_points}")
+        return trajectory
 
     def _convert_data(self) -> None:
         """
